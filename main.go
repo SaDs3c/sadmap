cf109cb1b385f30428c1729ae24486576bd25d61
hzwedt@gmail.com <hzwedt@gmail.com>
2023-12-20 03:47:30 +0100

    added port.go

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
         "fmt"
+
+        "github.com/SaDs3c/sadmap/port"
 )
 
 func main() {
         fmt.Println("SadMap 0.1 - Powered by sadsec.")
+
+        open := port.ScanPort("tcp", "localhost", 1337)
+        fmt.Printf("Port Open: %t\n", open)
 }
